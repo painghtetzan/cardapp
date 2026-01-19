88b1549e2bb188ad34110e831a885cd413a3a05b
painghtetzan <54760615+painghtetzan@users.noreply.github.com>
2026-01-19 11:49:02 +0800

    starter

---
+++ card-app-starter-main/src/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import CardList from "./pages/CardList";
+import AddCard from "./pages/AddCard";
+import EditCard from "./pages/EditCard";
+import "./App.css";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cards" element={<CardList />} />
+        {/* TODO: Complete the routes */}
+      </Routes>
+    </BrowserRouter>
+  );
+}
---
+++ card-app-starter-main/src/pages/Home.js
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom";
+
+export default function Home() {
+  /* TODO: Design and complete the Home page
+    - display instructions
+    - link to Cards page
+    - style as a landing page */
+  return <main></main>;
+}
